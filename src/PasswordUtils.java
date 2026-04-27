cdd427233ecb0acf3751398222bc2673090ca013
uglybrecke <206612742+uglybrecke@users.noreply.github.com>
2026-04-27 13:58:29 -0700

    made the count special characters method

--- src/PasswordUtils.java
+++ src/PasswordUtils.java
@@ -77,11 +77,14 @@ public class PasswordUtils {
     * @return the count of special characters
     */
     public static int countSpecialCharacters(String password) {
-        //loop through
-        //isLetterOrDigit
-    // TODO: Make tests FIRST, then implement the method
-    // Please have your tests in a separate commit from the implementation
-    return 0;
+        int count = 0;
+        for (int i = 0; i <= password.length() - 1; i++) {
+            char c = password.charAt(i);
+            if (!Character.isLetterOrDigit(c)) {
+                count++;
+            }    
+        }
+        return count;
     }
 
     /**
