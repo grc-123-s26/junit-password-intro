100eec4f8c511c2d969ded7b518d01002ec95b5a
tk421 <42457203+tk421bsod@users.noreply.github.com>
2026-04-27 11:17:41 -0700

    Implement describePasswordLength tests

--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -1,8 +1,26 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordUtilsTest {
     @Test
     void testDescribePasswordLengthShortPassword() {
+        String password = "";
+        String result = PasswordUtils.describePasswordLength(password);
+        assertEquals("short", result);
+    }
 
+    @Test 
+    void testDescribePasswordLengthLongPassword() {
+        String password = "eeniemeeniemienemoe";
+        String actual = PasswordUtils.describePasswordLength(password);
+        assertEquals("long", actual);
+    }
+
+    @Test
+    void testPasswordLongerThan40charsIsVeryLong() {
+        String password = "qwertyuiopasdfghjklqwertyuoopzxvbnqwertqwertyuiopasdfghjklzxcvbnm";
+        String actual = PasswordUtils.describePasswordLength(password);
+        assertEquals("very long", actual);
     }
 }
