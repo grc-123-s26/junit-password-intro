6f81fb255a63bdf2bc1b12e49c6269369e8f4df1
uglybrecke <206612742+uglybrecke@users.noreply.github.com>
2026-04-27 11:11:32 -0700

    commited what we worked on in class

--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -1,8 +1,38 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordUtilsTest {
     @Test
-    void testDescribePasswordLengthShortPassword() {
-
+    void testDescribePasswordLengthShortPasswordEmptyString() {
+        //Arrange
+        String password = "";
+        //Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //Assert
+        assertEquals("short", actual);
     }
+
+    @Test
+    void testDescribePasswordLengthLongPassword() {
+        //Arrange
+        String password = "asdjkjfghjklasdfg";
+        //Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //Assert
+        assertEquals("long", actual);
+    }
+
+    @Test
+    void testPasswordLongerThan40charsIsVeryLong() {
+        //Arrange
+        String password = "qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm";
+        //Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //Assert
+        assertEquals("very long", actual);
+    }
+
+
+
 }
