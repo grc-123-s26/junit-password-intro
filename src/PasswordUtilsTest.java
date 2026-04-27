622de5b2fedec9f1330d1957c827cfc4554fb662
JustinS720 <274596103+JustinS720@users.noreply.github.com>
2026-04-27 11:12:08 -0700

    Added livecode testing, test for short, long, and very long

--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -1,8 +1,35 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordUtilsTest {
     @Test
-    void testDescribePasswordLengthShortPassword() {
+    void testDescribePasswordLengthShortPasswordEmptyString() {
+        // Arrange
+        String password = "";
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        // Assert
+        assertEquals("short", actual);
+    }
 
+    @Test
+    void testDescribePassWordLengthLongPassword() {
+        // Arrange
+        String password = "eeneemeeniemienemoe";
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        // Assert
+        assertEquals("long", actual);
+    }
+
+    @Test
+    void testPasswordLongerThan40charsIsEveryLong() {
+        // Arrange
+        String password = "qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm";
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //Assert
+        assertEquals("very long", actual);
     }
 }
